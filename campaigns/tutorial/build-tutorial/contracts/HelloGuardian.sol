a6e56e03310ead112d6eee841076484b0bc9567f
Andres Guzman <andresguzman@Andress-MacBook-Air.local>
2023-12-20 15:28:28 -0600

    correction

--- campaigns/tutorial/build-tutorial/contracts/HelloGuardian.sol
+++ campaigns/tutorial/build-tutorial/contracts/HelloGuardian.sol
@@ -5,7 +5,7 @@ contract HelloGuardian {
 
     /// @return "Hello Guardian"
     function hello() external pure returns (string memory) {
-        return "Hello Guardians";
+        return "Hello Guardian";
     }
 
 }
